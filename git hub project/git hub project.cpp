29a73bc15e8797d63b66f96aca5abad6af76ffb6
trey <treyatlunch@gmail.com>
2025-08-28 10:33:23 -0500

    fixed it

---
+++ git hub project/git hub project.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+int main()
+{
+	std::cout << "wassup";
+
+	std::cout << " ayoooo\n";
+    std::cout << " ayoooo\n";
+
+	std::cout << "more words\n";
+	std::cout << "more words\n";
+
+}
+//fixed it time for the moneyssssss
